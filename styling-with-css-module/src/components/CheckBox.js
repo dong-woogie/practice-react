fcf56b60f128c99906dad2384f605bd989329318
dong-woogie <com6511@gmail.com>
2020-09-11 03:18:34 +0900

    css module을 사용해서 커스텀 체크박스 만들기

---
+++ styling-with-css-module/src/components/CheckBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+import classNames from "classnames/bind";
+import styles from "./CheckBox.module.css";
+
+const cx = classNames.bind(styles);
+
+function CheckBox({ children, checked, ...rest }) {
+  return (
+    <div className={cx("checkbox")}>
+      <label>
+        <input type="checkbox" checked={checked} {...rest} />
+        <div className={cx("icon")}>
+          {checked ? (
+            <MdCheckBox className={cx("checked")} />
+          ) : (
+            <MdCheckBoxOutlineBlank />
+          )}
+        </div>
+      </label>
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export default CheckBox;
