b3d2ab49103b4fbfa91adc4c82490b99106963f6
dong-woogie <com6511@gmail.com>
2020-09-22 01:30:16 +0900

    request한 데이터 다시 요청하지 않도록 캐싱하기

--- learn-redux-middleware/src/containers/PostContainer.js
+++ learn-redux-middleware/src/containers/PostContainer.js
@@ -2,13 +2,17 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPost } from "../modules/posts";
 import Post from "../components/Post";
+import { reducerUtils } from "../utils/asyncUtils";
 
 function PostContainer({ id }) {
   const dispatch = useDispatch();
-  const { data, error, loading } = useSelector((state) => state.posts.post);
+  const { data, error, loading } = useSelector((state) =>
+    state.posts.post[id] ? state.posts.post[id] : reducerUtils.initial()
+  );
   useEffect(() => {
+    if (data) return;
     dispatch(getPost(id));
-  }, [dispatch, id]);
+  }, [dispatch, id, data]);
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러발생!</div>;
--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -13,15 +13,68 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
-const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
-const getPostReducer = handleAsyncActions(GET_POST, "post");
-
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+// export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST, meta: id });
+  try {
+    const payload = await postsAPI.getPostById(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      meta: id,
+      payload,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_POST_ERROR,
+      meta: id,
+      error: true,
+      payload: e,
+    });
+  }
+};
+
+const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
+// const getPostReducer = handleAsyncActions(GET_POST, "post");
+
+const getPostReducer = (state, action) => {
+  const id = action.meta;
+  switch (action.type) {
+    case GET_POST:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.loading(
+            state.post[id] ? state.post[id].data : null
+          ),
+        },
+      };
+    case GET_POST_SUCCESS:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.success(action.payload),
+        },
+      };
+    case GET_POST_ERROR:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          [id]: reducerUtils.error(action.payload),
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  post: {},
 };
 
 export default function posts(state = initialState, action) {
