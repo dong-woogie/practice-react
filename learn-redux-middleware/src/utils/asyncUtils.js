a68d896be659540529f50c1391e422d526622a01
dong-woogie <com6511@gmail.com>
2020-09-21 16:27:14 +0900

    posts reducer 추가

---
+++ learn-redux-middleware/src/utils/asyncUtils.js
@@ -0,0 +1,70 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (params) => async (dispatch) => {
+    dispatch({ type });
+    try {
+      const payload = await promiseCreator(params);
+      console.log(payload);
+      dispatch({
+        type: SUCCESS,
+        payload,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+      });
+    }
+  };
+};
+
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
+export const reducerUtils = {
+  initial: (data = null) => ({
+    loading: false,
+    data,
+    error: null,
+  }),
+  loading: (prevState = null) => ({
+    data: prevState,
+    loading: true,
+    error: null,
+  }),
+  success: (data) => ({
+    data,
+    error: null,
+    loading: false,
+  }),
+  error: (error) => ({
+    error,
+    data: null,
+    loading: false,
+  }),
+};
