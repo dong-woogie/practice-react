8e8bee8264dc141e33fc6a97aa896ab12b627220
dong-woogie <com6511@gmail.com>
2020-09-23 14:59:44 +0900

    json-server를 사용해서 실제 api요청

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,32 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-// { id, title, body}
-
-const posts = [
-  {
-    id: 1,
-    title: "redux middle",
-    body: "리덕스 미들웨어 만들기!",
-  },
-  {
-    id: 2,
-    title: "redux thunk",
-    body: "redux-thunk배우기!",
-  },
-  {
-    id: 3,
-    title: "redux saga",
-    body: "redux-saga 배우기!",
-  },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get("/posts");
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-
-  return posts.find((post) => post.id === +id);
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
 };
